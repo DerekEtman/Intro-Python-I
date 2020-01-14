475c1e0a044dcf5d9822c2c76042c2d77039aa81
Derek Etman <dereketman@gmail.com>
2020-01-14 13:29:09 -0800

    + 12_scope

--- src/12_scopes.py
+++ src/12_scopes.py
@@ -5,7 +5,8 @@
 x = 12
 
 def change_x():
-    x = 99
+   x = 99
+   print(x)
 
 change_x()
 
@@ -19,7 +20,9 @@ def outer():
     y = 120
 
     def inner():
+        nonlocal y
         y = 999
+        print("inner", y)
 
     inner()
 
